12a177e8087a64a4c1569c1e2a1550d01e1f0573
Abay <ibrayev.abay@gmail.com>
2023-12-10 20:43:26 -0700

    lesson

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index_baby, name='index'),
+    path('britney/', views.hit_me_baby_one_more_time, name='britney')
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index_baby(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, worldz. You're at the polls index.")
+
+def hit_me_baby_one_more_time(request):
+    return HttpResponse("It's Britney bish.")
